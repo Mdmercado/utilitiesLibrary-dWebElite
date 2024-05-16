6a2cc6a5f997341a514a981af033dfc0e0480a0e
Juan Manuel Mogliati <mogliatijm@gmail.com>
2024-05-16 20:01:44 -0300

    chore: :recycle: add comments for stryker mutations

--- src/validators/Arrays/getUniqueValues.ts
+++ src/validators/Arrays/getUniqueValues.ts
@@ -1,6 +1,7 @@
 import { isEmpty } from './isEmpty'
 
 export function getUniqueValues<T>(input: T[]): T[] {
+  // Stryker disable next-line ConditionalExpression: The true mutant results in an empty array and false mutant follow the next line
   if (isEmpty(input)) return []
   const uniqueSet = new Set(input)
   return Array.from(uniqueSet)
